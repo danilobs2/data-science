9b408dfcc819e77e1504b793f5e6530bb635ad2b
Danilo Barros <112998481+danilobs2@users.noreply.github.com>
2022-10-03 00:06:27 -0300

    Update Deafio_Calculo de viagem.py

--- Deafio_Calculo de viagem.py
+++ Deafio_Calculo de viagem.py
@@ -9,5 +9,30 @@
 # Abaixo segue um exemplo de código que você pode ou não utilizar
 valores = input().split()
 
-# TODO:  Calcule quantidade de litros necessária para realizar a viagem e imprima com TRÊS dígitos após o ponto decimal
+# TODO: 
 '''
+
+'''
+Desafio
+Rubens quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem de carro, sendo que seu carro faz 12 KM/L.
+Como ele não sabe fazer um programa que o auxilie nessa missão, ele te pede ajuda.
+Para efetuar o cálculo, deve-se fornecer o tempo gasto em horas na viagem e a velocidade média durante a mesma em km/h.
+Assim, você conseguirá passar para Rubens qual a distância percorrida e, em seguida, calcular quantos litros serão necessários para a viagem 
+que ele quer fazer. Mostre o valor com 3 casas decimais após o ponto.
+
+Entrada
+O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem em horas e o segundo é a velocidade média durante a mesma em km/h.
+'''
+# Calcule a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal:
+
+KM_LITRO = 12
+
+valores = input().split()
+
+tempoGasto = float(valores[0])
+velocidadeMedia = float(valores[1])
+
+distancia = tempoGasto*velocidadeMedia
+consumo = distancia / 12
+
+print(f"{consumo:.3f}")
